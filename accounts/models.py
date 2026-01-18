a2b9084e157e8bf433d84c42ccd2a8283176838d
Aman Gupta <amangupta777a@gmail.com>
2026-01-18 14:39:28 +0530

    Fix Django admin registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class user(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('manager', 'Manager'),
+        ('sales', 'Sales'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
 
 # Create your models here.
--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+
+    def __str__(self):
+       return str(self.name) or ""
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return str(self.name) or ""
 # Create your models here.
